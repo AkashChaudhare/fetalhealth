deabda7a8f6688ff8a1d5e6fe7a767a7076617c9
AkashChaudhare <akash.chaudhare@gmail.com>
2023-02-19 23:49:36 +0530

    .

--- fetalhealth/settings.py
+++ fetalhealth/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d%vrt5bei(vn7a941osgrbpee-19b_z8-bjhrau&zc%@@23m*$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
